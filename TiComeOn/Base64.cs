aa7a562c9ecd2fc2bdbc1175189525c9d49448d8
某10 <0w0@0w0.pw>
2020-06-20 21:12:32 +0800

    Add GetSSRLinkForServer

---
+++ TiComeOn/Base64.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiCome
+{
+    public static class Base64
+    {
+        public static string DecodeBase64(string val)
+        {
+            return Encoding.UTF8.GetString(Base64.DecodeBase64ToBytes(val));
+        }
+        public static byte[] DecodeBase64ToBytes(string val)
+        {
+            string s = val.PadRight(val.Length + (4 - val.Length % 4) % 4, '=');
+            return Convert.FromBase64String(s);
+        }
+        public static string EncodeUrlSafeBase64(byte[] val, bool trim)
+        {
+            if (trim)
+            {
+                return Convert.ToBase64String(val).Replace('+', '-').Replace('/', '_').TrimEnd(new char[]
+                {
+                    '='
+                });
+            }
+            return Convert.ToBase64String(val).Replace('+', '-').Replace('/', '_');
+        }
+        public static byte[] DecodeUrlSafeBase64ToBytes(string val)
+        {
+            string s = val.Replace('-', '+').Replace('_', '/').PadRight(val.Length + (4 - val.Length % 4) % 4, '=');
+            return Convert.FromBase64String(s);
+        }
+        public static string EncodeUrlSafeBase64(string val, bool trim = true)
+        {
+            return Base64.EncodeUrlSafeBase64(Encoding.UTF8.GetBytes(val), trim);
+        }
+        public static string DecodeUrlSafeBase64(string val)
+        {
+            return Encoding.UTF8.GetString(Base64.DecodeUrlSafeBase64ToBytes(val));
+        }
+        public static string DecodeStandardSSRUrlSafeBase64(string val)
+        {
+            if (val.IndexOf('=') >= 0)
+            {
+                throw new FormatException();
+            }
+            return Encoding.UTF8.GetString(Base64.DecodeUrlSafeBase64ToBytes(val));
+        }
+    }
+}
---
+++ TiComeOn/Server.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiCome.Data;
+
+namespace TiCome
+{
+    public static class Server
+    {
+        public static string GetSSRLinkForServer(NodeConfig nodeConfig)
+        {
+            string main_part = nodeConfig.server + ":" + nodeConfig.server_port + ":" + nodeConfig.protocol + ":" + nodeConfig.method + ":" + nodeConfig.obfs + ":" + Base64.EncodeUrlSafeBase64(nodeConfig.password);
+            string param_str = "obfsparam=" + Base64.EncodeUrlSafeBase64(nodeConfig.obfsparam ?? "");
+            if (!string.IsNullOrEmpty(nodeConfig.protocolparam))
+            {
+                param_str += "&protoparam=" + Base64.EncodeUrlSafeBase64(nodeConfig.protocolparam);
+            }
+            if (!string.IsNullOrEmpty(nodeConfig.remarks))
+            {
+                param_str += "&remarks=" + Base64.EncodeUrlSafeBase64(nodeConfig.remarks);
+            }
+            if (!string.IsNullOrEmpty(nodeConfig.group))
+            {
+                param_str += "&group=" + Base64.EncodeUrlSafeBase64(nodeConfig.group);
+            }
+            if (nodeConfig.udp_over_tcp)
+            {
+                param_str += "&uot=" + "1";
+            }
+            if (nodeConfig.server_udp_port > 0)
+            {
+                param_str += "&udpport=" + nodeConfig.server_udp_port.ToString();
+            }
+            string base64 = Base64.EncodeUrlSafeBase64(main_part + "/?" + param_str);
+            return "ssr://" + base64;
+        }
+    }
+}
